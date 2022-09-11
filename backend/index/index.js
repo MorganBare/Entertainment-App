85889f7c82c3f60c73c2e5c829d04b6917345d7b
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 19:31:55 +0100

    Moved variables
    I reordered variables

--- backend/index/index.js
+++ backend/index/index.js
@@ -1,15 +1,22 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
 const app = express();
+
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 
-const authRouter = require("../routes/auth/auth");
+const PORT = process.env.PORT || 5000;
+
+const mongoose = require("mongoose");
+
+const authRouter = require("../routes/auth/authRouter");
 app.use("/auth", authRouter);
 
-const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
+  if (err) throw err;
+  console.log("Connected to MongoDB");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
