48613da46690f0c3220d08ccd4bdf43ee77fe342
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 14:03:18 +0100

    Export and render SignUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import { SignUp } from "./Components";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <SignUp />
+    </>
   );
 }
 
---
+++ frontend/src/Components/index.js
@@ -0,0 +1,3 @@
+import SignUp from "./Form/SignUp/SignUp";
+
+export { SignUp };
