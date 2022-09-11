84f814c03f3f72f5d399da3e3c8eb1ee5499a104
Morgan <morganjbare@gmail.com>
2022-09-11 19:30:51 +0200

    Added theme and global CSS reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import Routing from "./Routing/Routing";
+import { ThemeProvider } from "styled-components";
+import theme from './theme/theme';
+import GlobalStyle from './theme/globalStyle';
 
 function App() {
   return (
     <>
+     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Routing />
+     </ThemeProvider>
     </>
   );
 }
---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+    font: {
+      family: "'Outfit Light', monospace",
+      base: '1rem'
+    },
+    colors: {
+      red: '#FC4747',
+      darkBlue: '#10141E',
+      greyishBlue: '#5A698F',
+      semiDarkBlue: '#161D2F',
+      white: '#fff'  
+    },
+    spacing: {
+      default: '20px',
+      half: '10px',
+      quarter: '5px',
+      double: '40px',
+    },
+  };
+  
+  export default theme;
