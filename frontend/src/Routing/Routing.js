d6ab842d9e89afdf9265d161f0a17206d0139a5c
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 19:27:07 +0100

    Renamed routes

--- frontend/src/Routing/Routing.js
+++ frontend/src/Routing/Routing.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SignUp, Login } from "Components";
+import { Register, Login } from "Components";
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/sign-up" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
