e918cb6d129b30cbe69c25c0dcc9e0493e23546a
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 14:04:03 +0100

    Created Signup component
    Have a good look through the styled comps for this file. Some of the css that is needed I had to google.

--- frontend/src/Components/Form/SignUp/SignUp.jsx
+++ frontend/src/Components/Form/SignUp/SignUp.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Wrapper,
+  Container,
+  Input_Container,
+  Form,
+  Header,
+  Form_Input,
+  Form_Button,
+  Form_Text,
+} from "../Form.styled";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header>Sign Up</Header>
+        <Form>
+          <Input_Container>
+            <Form_Input placeholder="Email address" type="email" required />
+          </Input_Container>
+
+          <Input_Container>
+            <Form_Input placeholder="Password" type="password" required />
+          </Input_Container>
+
+          <Input_Container>
+            <Form_Input placeholder="Repeat Password" required />
+          </Input_Container>
+
+          <Form_Button type="submit">Create an account </Form_Button>
+
+          <Form_Text>
+            Already have an account? <a to="/sign-in">Sign In</a>
+          </Form_Text>
+        </Form>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default SignUp;
