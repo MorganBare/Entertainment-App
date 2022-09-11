54c6c05c50ceffee2d0dab1f5ef9b0eb50642cc8
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 19:28:35 +0100

    Created Login logic

--- frontend/src/Components/Form/Login/Login.jsx
+++ frontend/src/Components/Form/Login/Login.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { decodeToken } from "react-jwt";
+
 import {
   Wrapper,
   Container,
@@ -12,20 +15,54 @@ import {
 } from "../Form.styled";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/auth/login",
+        user
+      );
+
+      if (data.status === "success") {
+        localStorage.setItem("token", data.token);
+        const decodedToken = decodeToken(data.token);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Wrapper>
       <Container>
         <Header>Login</Header>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Input_Container>
-            <Form_Input placeholder="Email address" type="email" required />
+            <Form_Input
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              placeholder="Email address"
+              type="email"
+              required
+            />
           </Input_Container>
 
           <Input_Container>
-            <Form_Input placeholder="Password" type="password" required />
+            <Form_Input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              placeholder="Password"
+              type="password"
+              required
+            />
           </Input_Container>
 
-          <Form_Button type="submit">Create an account </Form_Button>
+          <Form_Button type="submit">Login to your account </Form_Button>
 
           <Form_Text>
             Don't have an account? <a to="/sign-in">Sign up</a>
