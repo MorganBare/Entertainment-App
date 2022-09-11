2392f87a412b3dc6dc291ad25149ca69f98f4ea7
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 19:26:23 +0100

    Created User Schema

---
+++ backend/models/UserModel.js
@@ -0,0 +1,29 @@
+// create a new mongoose schema
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
