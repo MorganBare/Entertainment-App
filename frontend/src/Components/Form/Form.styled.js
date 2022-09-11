2f8a6a89597a93537aa78572b61a79e76233ca9b
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-11 12:14:31 +0100

    Created form styled comps

---
+++ frontend/src/Components/Form/Form.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Form = styled.form`
+  background-color: #161d2f;
+`;
+
+export const Header = styled.h1`
+  font-size: 32px;
+  line-height: 40px;
+  color: #fff;
+`;
+
+export const Form_Label = styled.label`
+  color: #8b8e97;
+`;
+
+export const Form_Input = styled.input``;
+
+export const Form_Button = styled.button`
+  background-color: #fc4747;
+`;
+
+export const Form_Text = styled.p``;
