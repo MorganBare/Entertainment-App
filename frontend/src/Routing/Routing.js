5dbb4776c346bfa414bd4e9f73306f87a4192b69
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-13 14:41:37 +0100

    Added hero to routing for testing

--- frontend/src/Routing/Routing.js
+++ frontend/src/Routing/Routing.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Register, Login } from "Components";
+import { Hero } from "Components";
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Hero />} />
         <Route path="/sign-up" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
