521079378ddaad08288dbfb546cef219d15a55ef
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-13 14:41:10 +0100

    Created card

---
+++ frontend/src/Components/Card/Card.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  CardContainer,
+  ImageContainer,
+  CardImage,
+  CardTitle,
+  CardContent,
+  Info,
+} from "./Card.styled.js";
+
+const Card = ({ item }) => {
+  const {
+    title,
+    name,
+    release_date,
+    media_type,
+    overview,
+    poster_path,
+    original_language,
+    adult,
+  } = item;
+  const imagePrefix = "http://image.tmdb.org/t/p/w500";
+
+  console.log(item);
+  return (
+    <CardContainer>
+      <ImageContainer>
+        <CardImage src={`${imagePrefix}${poster_path}`} />
+      </ImageContainer>
+      <CardContent>
+        <Info>Release: {release_date}</Info>
+        <Info>Lan: {original_language}</Info>
+        <Info>{overview}</Info>
+        <Info>{media_type}</Info>
+        <Info>{adult ? "18+" : "PG"}</Info>
+      </CardContent>
+      <CardTitle>{title ?? name}</CardTitle>
+    </CardContainer>
+  );
+};
+
+export default Card;
---
+++ frontend/src/Components/Card/Card.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  -webkit-box-shadow: 0px 0px 15px -4px #fff;
+  box-shadow: 0px 0px 15px -8px #fff;
+  border-radius: 5px;
+  flex: 1 0 20%;
+`;
+export const ImageContainer = styled.div`
+  margin: 0 auto;
+`;
+export const CardImage = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+export const CardContent = styled.div`
+  color: #fff;
+`;
+export const Info = styled.span`
+  color: #fff;
+  display: block;
+`;
+export const CardTitle = styled.h3`
+  color: #fff;
+`;
