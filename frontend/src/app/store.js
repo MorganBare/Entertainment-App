ce0a08ece55a5f991b5ea5a0bc11a1273cce4878
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-15 08:49:24 +0100

    renamed file

--- frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "Features/user/userSlice";
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
