a648b9902c28a11794c77a210407adfd80619fe8
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-15 11:40:11 +0100

    created sidebar

--- frontend/src/Components/SideBar/SideBar.styled.js
+++ frontend/src/Components/SideBar/SideBar.styled.js
@@ -3,4 +3,15 @@ import styled from "styled-components";
 export const SideBarContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.semiDarkBlue};
   border-radius: 10px;
+  min-height: 80vh;
+  margin-top: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em 0 0 0;
+  justify-content: space-around;
+
+  svg {
+    transform: scale(1);
+  }
 `;
