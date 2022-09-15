4767bae465c81bbd77bde68fbd0c9012a4e866d8
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-15 11:06:10 +0100

    export comps

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -3,5 +3,7 @@ import Login from "./Form/Login/Login";
 import Card from "./Card/Card";
 import Hero from "./Hero/Hero";
 import ProtectedRoute from "./Protected_Routes/ProtectedRoute";
+import SideBar from "./SideBar/SideBar";
+import Search from "./Search/Search";
 
-export { Register, Login, Card, Hero, ProtectedRoute };
+export { Register, Login, Card, Hero, ProtectedRoute, SideBar, Search };
