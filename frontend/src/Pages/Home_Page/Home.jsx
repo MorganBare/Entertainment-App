cab28ecff71848af86902d7b485d899f1a054143
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-15 10:25:21 +0100

    Tested the functions
    I have tested the functions and they work well.
    
    To try them out just go to each route and refresh the page. The order will change.

--- frontend/src/Pages/Home_Page/Home.jsx
+++ frontend/src/Pages/Home_Page/Home.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Hero } from "../../Components";
 import { getTrending } from "../../utils/API_Functions/getData";
+import { randomizeData } from "../../utils/API_Functions/getRandomData";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
-  const data = async () => {
-    const trending = await getTrending("all", "week");
-    setMovies(trending);
 
-    console.log(movies);
+  const data = async () => {
+    // Same data function as in TVSeries.jsx
+    const trending = await getTrending("all", "week");
+    // Same random function as in TVSeries.jsx
+    setMovies(randomizeData(trending));
   };
   useEffect(() => {
     data();
--- frontend/src/Pages/TV_Series_Page/TVSeries.jsx
+++ frontend/src/Pages/TV_Series_Page/TVSeries.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Hero } from "../../Components";
 import { getTrending } from "../../utils/API_Functions/getData";
+import { randomizeData } from "../../utils/API_Functions/getRandomData";
+
 const TVSeries = () => {
   const [movies, setMovies] = useState([]);
+
   const data = async () => {
     const trending = await getTrending("tv", "day");
-    setMovies(trending);
+    setMovies(randomizeData(trending));
   };
   useEffect(() => {
+    // Because the call to the data() function is inside a useEffect, every time the page loads, the order of the TV Series will be random.
+
+    // Go to http://localhost:3000/tv-series and refresh the page to see the random TV Series.
     data();
   }, []);
   return <Hero movies={movies} title="TV Series" />;
