4101fe66ff56a377f0cd4d9e70af647403e752b4
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-15 11:39:59 +0100

    imported svgs

--- frontend/src/Components/SideBar/SideBar.jsx
+++ frontend/src/Components/SideBar/SideBar.jsx
@@ -3,24 +3,28 @@ import { SideBarContainer } from "./SideBar.styled";
 import { ReactComponent as Movies } from "../../assets/icon-nav-movies.svg";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { ReactComponent as BookMark } from "../../assets/icon-nav-bookmark.svg";
-import { ReactComponent as TV } from "../../assets/icon-category-tv.svg";
+import { ReactComponent as TV } from "../../assets/icon-nav-tv-series.svg";
+import { ReactComponent as Home } from "../../assets/icon-nav-home.svg";
 
 const SideBar = () => {
   return (
     <SideBarContainer>
       <div>
-        <Movies />
+        <Logo />
       </div>
       <div>
-        <Logo />
+        <Home />
+      </div>
+      <div>
+        <Movies />
+      </div>
+
+      <div>
+        <TV />
       </div>
       <div>
         <BookMark />
       </div>
-      <div>
-        <TV />
-      </div>
-      <div></div>
       <div></div>
     </SideBarContainer>
   );
