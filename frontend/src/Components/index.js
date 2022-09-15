b26a36040adb09495522c68320b1f8081bd58125
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-15 08:51:13 +0100

    default exports

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -2,5 +2,6 @@ import Register from "./Form/SignUp/Register";
 import Login from "./Form/Login/Login";
 import Card from "./Card/Card";
 import Hero from "./Hero/Hero";
+import ProtectedRoute from "./Protected_Routes/ProtectedRoute";
 
-export { Register, Login, Card, Hero };
+export { Register, Login, Card, Hero, ProtectedRoute };
