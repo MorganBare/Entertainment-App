9c70c666fc1bcedab15f51551f7bdb77c1b2e389
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-17 13:37:43 +0100

    Created media query for the layout

--- frontend/src/Components/Layout/Layout.styled.js
+++ frontend/src/Components/Layout/Layout.styled.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const LayoutContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 11fr;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
