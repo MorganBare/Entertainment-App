b6e39d1d20783b61f2beda11d9abdbf6c75cec70
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-17 14:25:57 +0100

    added reducer to the store

--- frontend/src/Redux/app/store.js
+++ frontend/src/Redux/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "Redux/Slices/user/userSlice";
+import bookMarkReducer from "Redux/Slices/bookmark/bookSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    bookMark: bookMarkReducer,
   },
 });
