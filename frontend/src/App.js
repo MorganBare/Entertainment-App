88a758dfd6bf9007f888401fdd8577750d2fa4f2
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-09-18 11:38:22 +0100

    imported actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Routing from "./Routing/Routing";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme/theme";
 import GlobalStyle from "./theme/globalStyle";
+import {
+  fetchTrending,
+  fetchTvSeries,
+  fetchMovies,
+} from "./Redux/Slices/bookmark/bookMarkSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTrending());
+    dispatch(fetchTvSeries());
+    dispatch(fetchMovies());
+  }, []);
   return (
     <>
       <ThemeProvider theme={theme}>
